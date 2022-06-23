3bf1ac25b058a9d25c18d185066d63f8ab088c77
jlcalda <jlcaldadev23@gmail.com>
2022-06-24 07:42:22 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import { Navbar } from './components/Navbar';
+import { Routes } from './components/Routes';
+import { Footer } from './components/Footer';
+
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="bg-gray-500">App</div>
+    </div>
+  );
+};
+
+export default App;
